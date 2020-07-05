9777044b75e301ed0e774806c0fbccba1cc0da69
shandysiswandi <shandysiswandi@gmail.com>
2020-07-05 18:43:09 +0700

    init saw

---
+++ src/Saw.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ngutex\Elparithm;
+
+use Ngutex\Elparithm\Abstracts\Algorithm;
+
+class Saw extends Algorithm
+{
+    private $alternatives;
+    private $criterias;
+    private $weights;
+}
